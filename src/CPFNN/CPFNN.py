a9ae8187a3e0f641581d62829184b5da5874c0e0
BenjaminGustafson <bgustafs@trinity.edu>
2021-06-02 22:24:49 -0500

    trying to fix discrepencies with prefilter.py

--- src/CPFNN/CPFNN.py
+++ src/CPFNN/CPFNN.py
@@ -5,10 +5,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import correlation
+import time
 
 # settings
 class Config(object):
-    #Common dimentions (CpG sites) we used: 473034 303236 9607 8105 8195. 
+    #Common dimensions (CpG sites) we used: 473034 303236 9607 8105 8195. 
     input_dim = 473034 #the number of biological makers
     hidden_dim = 200   #the number of neuros in hidden layer 
     train_file_path = '/data/zhanglab/lli1/methylation/train_combat.csv' #training file path
@@ -34,18 +35,14 @@ class neural_network_CPFNN(nn.Module):
 
     #initialize weights
     def init_weights(self):
-        nn.init.xavier_normal(self.fc1.weight) #initialize first layer weight Xavier initialization
-        nn.init.xavier_normal(self.fc2.weight) #initialize second layer weight Xavier initialization
+        init.xavier_normal(self.fc1.weight) #initialize first layer weight Xavier initialization
+        init.xavier_normal(self.fc2.weight) #initialize second layer weight Xavier initialization
     
     #forward propogation
-    def forward(self, x_in, corr, indexes, counter, apply_softmax=False):
+    def forward(self, x_in, apply_softmax=False):
        
         a_1 = F.leaky_relu(self.fc1(x_in))  # activaton function added!
         y_pred = F.leaky_relu(self.fc2(a_1)) #prediction
-        self.l1_penalty = torch.norm(self.fc1.weight,1) #calculating l1 penality, using norm 1 (absolute value norm)
-        self.l2_penalty = torch.norm(self.fc1.weight,2) #calculating l2 penality, using norm 2 (Euclidean norm)
-        self.corr_l1_penalty = torch.sum(torch.sum(torch.abs(self.fc1.weight), dim = 0)[indexes]) #corr lasso (l1) penality
-        self.corr_l2_penalty = torch.sum(torch.sum((self.fc1.weight)**2, dim = 0)[counter]) #corr ridge (l2) penality
         if apply_softmax:
             y_pred = F.softmax(y_pred, dim=1) #apply softmax to the prediction
 
@@ -62,31 +59,7 @@ class Trainer(object):
         self.batch_size = batch_size #initialize batch size
     
     #online training (one by one training)
-    def train_one_by_one(self,x_train,y_train, alpha=0.0, l1_ratio=0.0): #x_train is input features, y_train is labels, alpha is the penalty, l1_ratio is the perentage of l1 penalty in alpha
-        for t in range(0,self.epoch):
-            loss = 0 #initialize loss
-            correct = 0 #initialize number of correct prediction
-            for i in range(0,len(x_train)): #loop through all training samples
-                y_pred = self.model(x_train[i,:], correlation) #get prediction from neural network
-                #Accuracy
-                if abs(y_pred - y_train[i,:]) < 3: # age difference < 3
-                    correct += 1
-                # Loss
-                #penalty = alpha*(l1_ratio*self.model.l1_penalty+(1-l1_ratio)*self.model.l2_penalty)
-                loss = self.loss_fn(y_pred, y_train[i,:]) # +penalty
-                # Zero all gradients
-                self.optimizer.zero_grad() # disgard all previous gradients
-                #Backward propogation
-                loss.backward()
-                # Update weights
-                self.optimizer.step()
-            # Verbose
-            if (t%20==0):
-                # Print the gredient
-                print(self.model.fc1.weight.grad)
-                print ("epoch: {0:02d} | loss: {1:.2f} | acc: {2:.1f}".format(t, loss, correct / len(y_train)))
-    #batch training
-    def train_by_random(self, train, correlation, indexes, complement_index, alpha=0.0, beta = 0.0,  l1_ratio=0.0):
+    def train_by_random(self, train):
         for t in range(0,self.epoch):
             from math import ceil
             loss = 0 #initialize loss
@@ -102,24 +75,19 @@ class Trainer(object):
                 start_index = i*self.batch_size 
                 end_index = (i+1)*self.batch_size if (i+1)*self.batch_size <= len(x_train) else len(x_train)
                 random_train = x_train[start_index:end_index,:] 
-                
+
                 y_labels = y_train[start_index:end_index].reshape(-1,1) #select batch of training labels
-                y_pred = self.model(random_train, correlation, indexes, complement_index) #get prediction from neural network
+                y_pred = self.model(random_train) #get prediction from neural network
                 acc +=  torch.sum(torch.abs(torch.sub(y_labels, y_pred))) #accuracy
-                
+
                 # Loss
-                penalty = alpha * self.model.corr_l1_penalty + beta * self.model.corr_l2_penalty 
-                loss = self.loss_fn(y_pred, y_labels)+penalty
-                
+                loss = self.loss_fn(y_pred, y_labels)
                 # Zero all gradients
-                self.optimizer.zero_grad() #disgard all previous gradients
-                
+                self.optimizer.zero_grad()
                 #Backward pass
                 loss.backward()
-                
                 # Update weights
                 self.optimizer.step()
-            # Verbose
             if (t%20==0):
                 print ("epoch: {0:02d} | loss: {1:.2f} | acc: {2:.2f}".format(t, loss, acc / len(y_train)))
 
@@ -129,10 +97,10 @@ class Trainer(object):
         correct = sum(list(map(lambda x: 1 if x<2 else 0,difference))) #regard prediction error within 2 years as correct and count correct prediction
         return correct
 
-    #testing
-    def test(self,x_test,y_test, correlation, indexes, complement_indexes):
+
+    def test(self,x_test,y_test):
         model = self.model.eval()
-        pred_test = model(x_test, correlation, indexes, complement_indexes) #get prediction from neural network
+        pred_test = model(x_test)
         #calculate mean absolute error of predicitons
         x = torch.cat([pred_test,y_test],1) 
         x_arr = x.cpu().data.numpy()
@@ -150,13 +118,21 @@ if torch.cuda.is_available():
 else:
     print("GPU is not available to use.\n")
 
-opt = Config()  
+opt = Config()
 
-print('alpha', opt.alpha)
-print('beta', opt.beta)
-print('epoch', opt.epoch_num)
 
-#select avaliable GPU/CPU device
+print('epoch = ', opt.epoch_num)
+print('learning_rate = ', opt.learning_rate)
+print('alpha = ', opt.alpha)
+print('beta = ', opt.beta)
+print('l1_ratio = ', opt.l1_ratio)
+print('batch_size = ', opt.batch_size)
+print('cor = ', opt.cor)
+print('num_sites = ', opt.num_sites)
+print('hidden_dim = ', opt.hidden_dim)
+print('train_file_path = ',  opt.train_file_path)
+print('test_file_path = ', opt.test_file_path) 
+
 device = None
 if opt.use_gpu and torch.cuda.is_available():
     device = torch.device('cuda')  
@@ -165,13 +141,13 @@ else:
     device = torch.device('cpu')
 
 print("Reading training data")
-train = np.loadtxt(opt.train_file_path, skiprows=1, delimiter=',') 
+train = np.loadtxt(opt.train_file_path, delimiter=',')
 
 print("Reading test data")
 test = np.loadtxt(opt.test_file_path, skiprows=1, delimiter=',')
 
 #calculate spearman and pearson correlation between CpG sites and age
-spearman_corr,pearson_corr = correlation.calculate_correlation(train) 
+spearman_corr,pearson_corr = correlation.calculate_correlation(train)
 
 #spearman: get index of correlation that above threshod and below threshold
 spearman_index = [x for x in range(len(spearman_corr)) if abs(spearman_corr[x])<opt.cor] #return all x that satisfy the if condition
@@ -195,13 +171,15 @@ y_train = train[:,0].reshape(-1,1)
 x_test = test[:,1:]
 y_test = test[:,0].reshape(-1,1)
 
-#build the neural network model
 model = neural_network_CPFNN(input_dim=opt.input_dim,hidden_dim=opt.hidden_dim,output_dim=opt.output_dim, indexes = spearman_index).to(device)
 
 print("Training")
-trainer = Trainer(epoch=opt.epoch_num,model=model,batch_size=opt.batch_size)
-trainer.train_by_random(train, spearman_corr, spearman_index, spearman_complement_index, alpha =  opt.alpha, beta = opt.beta, l1_ratio =  opt.l1_ratio )
-#test the neural network model
-print("Testing")
-trainer.test(x_test, y_test, spearman_corr, spearman_index, spearman_complement_index)
-
+start = time.time()
+trainer = Trainer(epoch=100,model=model,batch_size=50)
+for k in range(20):
+    trainer.train_by_random(sub_train)
+end = time.time()
+print("time elapsed (min) = ", (end-start)/60)
+trainer.test(x_test, y_test)
+output = model(x_test).data.numpy()
+np.savetxt('CPFNN_prediction1.txt', output,delimiter = ',')
