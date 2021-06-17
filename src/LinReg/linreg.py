1df3f4e32596e48788071ac1509e77d16019e029
BenjaminGustafson <bgustafs@trinity.edu>
2021-06-17 11:22:02 -0500

    add linreg.py

---
+++ src/LinReg/linreg.py
@@ -0,0 +1,65 @@
+import torch
+import matplotlib.pyplot as plt
+import math
+
+class LinearRegression:
+    def fit(self, X, y, method, learning_rate=0.01, iterations=500, batch_size=32):
+        X, y = torch.from_numpy(X), torch.from_numpy(y)
+        X = torch.cat([(X), torch.ones_like(y)], dim=1)
+        rows, cols = X.size()
+        if method == 'solve':
+            if rows >= cols == torch.matrix_rank(X):
+                self.weights = torch.matmul(
+                    torch.matmul(
+                        torch.inverse(
+                            torch.matmul(
+                                torch.transpose(X, 0, 1),
+                                X)),
+                        torch.transpose(X, 0, 1)),
+                    y)
+            else:
+                print('X has not full column rank. method=\'solve\' cannot be used.')
+        elif method == 'sgd':
+            self.weights = torch.normal(mean=0, std=1/cols, size=(cols, 1), dtype=torch.float64)
+            for i in range(iterations):
+                Xy = torch.cat([X, y], dim=1)
+                Xy = Xy[torch.randperm(Xy.size()[0])]
+                X, y = torch.split(Xy, [Xy.size()[1]-1, 1], dim=1)
+                for j in range(int(math.ceil(rows/batch_size))):
+                    start, end = batch_size*j, min(batch_size*(j+1), rows)
+                    Xb = torch.index_select(X, 0, torch.arange(start, end))
+                    yb = torch.index_select(y, 0, torch.arange(start, end))
+                    
+                    self.weights.requires_grad_(True)
+                    diff = torch.matmul(Xb, self.weights) - yb
+                    loss = torch.matmul(torch.transpose(diff, 0, 1), diff)
+                    loss.backward()
+                    
+                    self.weights = (self.weights - learning_rate*self.weights.grad).detach()
+        else:
+            print(f'Unknown method: \'{method}\'')
+        
+        return self
+    
+    def predict(self, X):
+        X = torch.from_numpy(X)
+        if not hasattr(self, 'weights'):
+            print('Cannot predict. You should call the .fit() method first.')
+            return
+        
+        X = torch.cat([X, torch.ones((X.size()[0], 1))], dim=1)
+        
+        if X.size()[1] != self.weights.size()[0]:
+            print(f'Shapes do not match. {X.size()[1]} != {self.weights.size()[0]}')
+            return
+        
+        return torch.matmul(X, self.weights)
+    
+    def rmse(self, X, y):
+        y = torch.from_numpy(y)
+        y_hat = self.predict(X)
+        
+        if y_hat is None:
+            return
+        
+        return torch.sqrt(torch.mean(torch.square(y_hat - y)))
